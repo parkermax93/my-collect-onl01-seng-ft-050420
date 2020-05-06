ad5e10a022f021281a889a0b819417bd6d0e8d0d
Parker Williamson <parker.williamson@gmail.com>
2020-05-06 20:05:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
-end
-
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang|
-  lang.upcase
-end
+def my_collect(collection)
+  i = 0 
+  new_collection = []
+  while i < collection.length 
+    new_collection << yield(collection[i])
+    i += 1
+  end
+new_collection
+end 
